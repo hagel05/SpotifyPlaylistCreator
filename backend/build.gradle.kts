eb27901e8fa4415e82ff5ace010c9778ddfe98a6
Matthew Hagel <hagel05@hotmail.com>
2026-04-26 16:40:58 -0500

    adds junit runner

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -20,6 +20,7 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
